11ea4f72e80b9a4861b555019e71304de483da48
oldflexer <imielientiev@gmail.com>
2026-09-07 00:00:39 +0500

    cleanup: Remove temporary fix scripts

--- fix_retry.py
+++
@@ -1,9 +0,0 @@
-with open('src/infrastructure/retry.py', 'r') as f:
-    content = f.read()
-
-content = content.replace('before_sleep=before_sleep_log(logger, "WARNING"),', 'before_sleep=before_sleep_log(logger, 30),')
-
-with open('src/infrastructure/retry.py', 'w') as f:
-    f.write(content)
-
-print('Fixed retry.py logging.WARNING')
